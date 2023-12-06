7ddddf68b43903e5d167fa9baccdcae97e3f3220
teixeirazeus <teixeira.zeus@gmail.com>
2023-12-06 18:16:50 -0300

    refact

--- day3.py
+++ day3.py
@@ -9,150 +9,112 @@
 # ...$.*....
 # .664.598.."""
 from input_data import input_data
-result = 4361
 
-result_part2 = 467835
+result_part1 = 549908
+result_part2 = 81166799
 
-engine_matrix = []
-def load_matrix():
-    engine_matrix = []
-    for line in input_data.split("\n"):
-        engine_line = []
-        for w in line:
-            engine_line.append(w)
-        engine_matrix.append(engine_line)
-    return engine_matrix
-engine_matrix = load_matrix()
+def print_engine(matrix):
+    for line in matrix:
+        print(line)
+
+def load_engine():
+    matrix = []
+    for line in input_data.splitlines():
+        matrix.append(list(line))
+    return matrix
+
+def load_blank_engine():
+    matrix = []
+    for line in input_data.splitlines():
+        matrix.append(list(line))
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            matrix[y][x] = "."
+    return matrix
+
+def is_symbol(value):
+    return not value.isdigit() and value != "."
+
+def get_full_number(x,y, blank_engine):
+    blank_engine[y][x] = engine[y][x]
     
-def explode_number(x_origin, y_origin):
-    # print("Explode", x_origin, y_origin, engine_matrix[y_origin][x_origin])
-    engine_matrix[y_origin][x_origin] = "."
-    x = x_origin
+    new_x = x
     while True:
-        x -= 1
-        if x < 0:
+        new_x -= 1
+        if new_x < 0:
             break
-        if engine_matrix[y_origin][x].isdigit():
-            engine_matrix[y_origin][x] = "."
+        if engine[y][new_x].isdigit():
+            blank_engine[y][new_x] = engine[y][new_x]
         else:
             break
-    x = x_origin
+    
+    new_x = x
     while True:
-        x += 1
-        if x >= len(engine_matrix[y_origin]):
+        new_x += 1
+        if new_x >= len(engine[y]):
             break
-        # print("Consulta", engine_matrix[y_origin][x])
-        if engine_matrix[y_origin][x].isdigit():
-            engine_matrix[y_origin][x] = "."
+        if engine[y][new_x].isdigit():
+            blank_engine[y][new_x] = engine[y][new_x]
         else:
             break
     
-def has_simbol_adjents(x, y):
-    adjents = grab_adjents(x, y)
-    for simbol in adjents:
-        if not simbol.isdigit() and simbol != ".":
-            return True
-    return False
+    return blank_engine
     
-def grab_adjents(x, y):
-    # print("Grab", x, y, engine_matrix[y][x], end=": ")
-    diretions = ((-1,1), (0,1), (1,1), 
-                 (-1,0), (1,0), 
-                 (-1,-1), (0,-1), (1,-1))
-    adjents = []
-    for x_add, y_add in diretions:
-        if (x_add+x) >= 0 and (y_add+y) >= 0 and x_add+x < len(engine_matrix[y]) and y_add+y < len(engine_matrix):
-            adjents.append(engine_matrix[y+y_add][x+x_add])
-    return adjents
-
-for y in range(len(engine_matrix)):
-    for x in range(len(engine_matrix[y])):
-        if has_simbol_adjents(x,y) and engine_matrix[y][x].isdigit():
-            explode_number(x,y)
-
-sum_total = 0
-for line in engine_matrix:
-    line = "".join(line).split(".")
-    for l in line:
-        if l.isdigit():
-            print(l, "is digit")
-            sum_total -= int(l)
-print(sum_total)
-
-engine_matrix = load_matrix()
-for y in range(len(engine_matrix)):
-    for x in range(len(engine_matrix[y])):
-        if not engine_matrix[y][x].isdigit():
-            engine_matrix[y][x] = "."
-
-for line in engine_matrix:
-    line = "".join(line).split(".")
-    print(">", line)
-    for l in line:
-        if l.isdigit():
-            sum_total += int(l)
             
-print(sum_total)
-print(result == sum_total)
+def scan_adj(x, y, blank_engine):
+    directions = ((-1,-1), (0, -1), (1,-1),
+                  (-1,0), (1,0),
+                  (-1,1), (0,1), (1,1))
+    for y_add, x_add in directions:
+        new_x = x + x_add
+        new_y = y + y_add
+        if new_x >= 0 and new_y >= 0 and new_x < len(engine[y]) and new_y < len(engine):
+            value = engine[new_y][new_x]
+            if value.isdigit():
+                blank_engine = get_full_number(new_x, new_y, blank_engine)
+    return blank_engine
+            
+
+engine = load_engine()
+blank_engine = load_blank_engine()
+
+for y in range(len(engine)):
+    for x in range(len(engine[y])):
+        if is_symbol(engine[y][x]):
+            blank_engine = scan_adj(x, y, blank_engine)
+            
+total = 0
+for line in blank_engine:
+    for c in "".join(line).split("."):
+        if c.isdigit():
+            total += int(c)
+            
+print_engine(blank_engine)
+
+print(total)
+print(total == result_part1)
+
+print("PART 2")
+engine = load_engine()
 
 
-def get_full_number(x_origin, y_origin, new_matrix):
-    x = x_origin
-    while True:
-        x -= 1
-        if x < 0:
-            break
-        if engine_matrix[y_origin][x].isdigit():
-            new_matrix[y_origin][x] = engine_matrix[y_origin][x]
-        else:
-            break
-    x = x_origin
-    while True:
-        x += 1
-        if x >= len(engine_matrix[y_origin]):
-            break
-        # print("Consulta", engine_matrix[y_origin][x])
-        if engine_matrix[y_origin][x].isdigit():
-            new_matrix[y_origin][x] = engine_matrix[y_origin][x]
-        else:
-            break
-    return new_matrix
-
-def grab_adjents_full_number(x, y):
-    new_matrix = make_new_matrix()
-    diretions = ((-1,1), (0,1), (1,1), 
-                 (-1,0), (1,0), 
-                 (-1,-1), (0,-1), (1,-1))
+def get_numbers(blank_engine):
     numbers = []
-    for x_add, y_add in diretions:
-        if (x_add+x) >= 0 and (y_add+y) >= 0 and x_add+x < len(engine_matrix[y]) and y_add+y < len(engine_matrix):
-            y_now = y+y_add
-            x_now = x+x_add
-            if engine_matrix[y_now][x_now].isdigit():
-                new_matrix[y_now][x_now] = engine_matrix[y_now][x_now]
-                new_matrix = get_full_number(x_now, y_now, new_matrix)
-    for line in new_matrix:
-        line = "".join(line).split(".")
-        for l in line:
-            if l.isdigit():
-                numbers.append(l)
+    for line in blank_engine:
+        for c in "".join(line).split("."):
+            if c.isdigit():
+                numbers.append(c)
     return numbers
 
-
-# parte 2
-engine_matrix = load_matrix()
-
-def make_new_matrix():
-    new_matrix = []
-    for line in engine_matrix:
-        new_matrix.append(["." for l in line])
-    return new_matrix
-
 total = 0
-for y in range(len(engine_matrix)):
-    for x in range(len(engine_matrix[y])):
-        if engine_matrix[y][x] == "*":
-            numbers = grab_adjents_full_number(x, y)
+
+for y in range(len(engine)):
+    for x in range(len(engine[y])):
+        if engine[y][x] == "*":
+            blank_engine = load_blank_engine()
+            blank_engine = scan_adj(x, y, blank_engine)
+            numbers = get_numbers(blank_engine)
+            print("Find start!", numbers)
             if len(numbers) == 2:
                 total += int(numbers[0]) * int(numbers[1])
 
