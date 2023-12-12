5781f926ff74c80b66ebe165d99d8daf92329624
teixeirazeus <teixeira.zeus@gmail.com>
2023-12-12 12:57:27 -0300

    day10 p2

--- day10.py
+++ day10.py
@@ -5,6 +5,27 @@ from input_data import input_data
 # .L-J.
 # ....."""
 
+# input_data = """...........
+# .S------7.
+# .|F----7|.
+# .||....||.
+# .||....||.
+# .|L-7F-J|.
+# .|..||..|.
+# .L--JL--J.
+# .........."""
+
+# input_data = """FF7FSF7F7F7F7F7F---7
+# L|LJ||||||||||||F--J
+# FL-7LJLJ||||||LJL-77
+# F--JF--7||LJLJ7F7FJ-
+# L---JF-JLJ.||-FJLJJ7
+# |F|F-JF---7F7-L7L|7|
+# |FFJF7L7F-JF7|JL---7
+# 7-L-JL7||F7|L7F-7F7|
+# L.L7LFJ|||||FJL7||LJ
+# L7JLJL-JLJLJL--JLJ.L"""
+
 DOWN = (1,0)
 UP = (-1,0)
 LEFT = (0,-1)
@@ -57,6 +78,12 @@ def is_inside_map(point):
     x_check = 0 <= x < len(mapp[0])
     return y_check and x_check
 
+def is_inside_map_arg(point, local_map):
+    y,x = point
+    y_check = 0 <= y < len(local_map)
+    x_check = 0 <= x < len(local_map[0])
+    return y_check and x_check
+
 def add_vector(v1, v2):
     return (v1[0]+v2[0], v1[1]+v2[1])
     
@@ -89,15 +116,17 @@ mapp = list(map(lambda x: list(x), input_data.splitlines()))
 
 start_point = get_start_point(mapp)
 max_distance = 0
+max_path = []
 
 def deep_search(path):
-    global max_distance
+    global max_distance, max_path
     if len(path) > 3 and is_start(path[-1]):
         for index, local in enumerate(path):
             distance = calculate_step_distance(start_point, local)
             if distance > max_distance:
                 max_distance = len(path)//2
-                print("New max:", max_distance )
+                max_path = path
+                print("New max:", max_distance)
         return
     for step in get_possible_directions(path[-1]):
         if step not in path[1:]:
@@ -111,7 +140,141 @@ import sys
 
 sys.setrecursionlimit(100+(len(mapp)*len(mapp[0])))
 deep_search([start_point])
+
+
+# p1
 # wrong 50
 # wrong 7178, too high
 # wrong 198
 # 6886
+
+import os
+import time
+
+print("Part 2")
+
+tile_to_matrix = {
+    "|": [[" ", "X", " "],
+          [" ", "X", " "],
+          [" ", "X", " "]],
+    "-": [[" ", " ", " "],
+          ["X", "X", "X"],
+          [" ", " ", " "]],
+    "L": [[" ", "X", " "],
+          [" ", "X", "X"],
+          [" ", " ", " "]],
+    "J": [[" ", "X", " "],
+          ["X", "X", " "],
+          [" ", " ", " "]],
+    "7": [[" ", " ", " "],
+          ["X", "X", " "],
+          [" ", "X", " "]],
+    "F": [[" ", " ", " "],
+          [" ", "X", "X"],
+          [" ", "X", " "]],
+    "S": [[" ", "X", " "],
+          ["X", "X", "X"],
+          [" ", "X", " "]],
+    ".": [[" ", " ", " "],
+          [" ", " ", " "],
+          [" ", " ", " "]],
+}
+
+# for p in max_path:
+#     print(p)
+#     mapp[p[0]][p[1]] = "X"
+#     os.system('clear')
+#     print_map(mapp)
+#     time.sleep(1)
+    
+drawn_mapp = []
+
+for y in range(len(mapp)*3):
+    new_line = []
+    for x in range(len(mapp[0])*3):
+        new_line.append(" ")
+    drawn_mapp.append(new_line)
+
+for y in range(len(mapp)):
+    for x in range(len(mapp[y])):
+        tile = mapp[y][x]
+        tile_matrix = tile_to_matrix[tile]
+        for y_m in range(len(tile_matrix)):
+            for x_m in range(len(tile_matrix[y_m])):
+                drawn_mapp[(y*3)+y_m][(x*3)+x_m] = tile_matrix[y_m][x_m]
+
+def flood_map(local_map):
+    local_map[0][0] = "~"
+    
+    used_water = True
+    while used_water:
+        used_water = False
+        for y, line in enumerate(local_map):
+            for x, value in enumerate(line):
+                if value == "~":
+                    for direction in (UP, DOWN, LEFT, RIGHT):
+                        step = add_vector((y,x), direction)
+                        if is_inside_map_arg(step, local_map):
+                            if local_map[step[0]][step[1]] not in ["X", "~"]:
+                                local_map[step[0]][step[1]] = "~"
+                                used_water = True
+    return local_map
+
+# def empty_count(real_map, local_map):
+#     for y, line in enumerate(real_map):
+#         for x, value in enumerate(real_map):
+#             for
+
+
+def get_result_matrix(y_start, x_start):
+    result_matrix = []
+    y = y_start*3
+    x = x_start*3
+    for y_m in range(y, y+3):
+        new_line = []
+        for x_m in range(x, x+3):
+            new_line.append(drawn_mapp[y_m][x_m])
+        result_matrix.append(new_line)
+    return result_matrix
+    
+def is_empty(matrix):
+    empty_count = 0
+    for line in matrix:
+        for value in line:
+            if value == " ":
+                empty_count += 1
+    return empty_count > 5
+        
+print_map(drawn_mapp)
+drawn_mapp = flood_map(drawn_mapp)
+print_map(drawn_mapp)
+count = 0
+
+for y in range(len(mapp)):
+    for x in range(len(mapp[y])):
+        result_matrix = get_result_matrix(y, x)
+        print_map(result_matrix)
+        print("-")
+        if is_empty(result_matrix):
+            count += 1
+print(count)
+# area = 0
+# for y, line in enumerate(drawn_mapp):
+#     found_x = False
+#     x_local = -1
+#     for x, tile in enumerate(line):
+#         if tile == "X" and not found_x:
+#             found_x = True
+#             x_local = x
+#             continue
+#         elif tile == "X" and found_x:
+#             if x_local == -1:
+#                 raise Exception("x_local is negative")
+#             found_x = False
+#             area += x-x_local-1
+# print(area)
+# print_map(drawn_mapp)
+
+
+# p2
+# 647 wrong, too high
