5ff2ff6e9ed99ce647d924724742c2ee4606f24c
teixeirazeus <teixeira.zeus@gmail.com>
2023-12-13 21:51:58 -0300

    day12 p2

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,4 +1,3 @@
-from itertools import permutations, combinations
 import multiprocessing
 
 def count_in_list(l,c):
@@ -8,30 +7,19 @@ def count_in_list(l,c):
             count += 1
     return count
 
-def make_perm(v):
-    hash_c = count_in_list(v,"#")
-    dot_c = count_in_list(v,".")
-    length = len(v)
-    s = []
-    
-    def perm_rec(v, hash_c, dot_c, length):
-        if len(v) == length:
-            return s.append(v)
-        if hash_c > 0:
-            perm_rec(v+["#"], hash_c-1, dot_c, length)
-        if dot_c > 0:
-            perm_rec(v+["."], hash_c, dot_c-1, length)
-    perm_rec([], hash_c, dot_c, length)
-    return s
-
 class HashCase:
     def __init__(self, line):
         self.debug_mode = False
+        self.part_two_mode = True
         self.case, self.solution = line.split()
-        self.case = self.case*5
-        self.solution += ","
-        self.solution *= 5
-        self.solution = self.solution[:-1]
+        
+        # part 2
+        if self.part_two_mode:
+            self.case = "?".join([self.case]*5)
+            self.solution += ","
+            self.solution *= 5
+            self.solution = self.solution[:-1]
+        
         self.case = list(self.case)
         self.solution = list(map(int, self.solution.split(",")))
         self.hash_count_case = 0
@@ -56,35 +44,34 @@ class HashCase:
             print("Try vector", self.try_vector)
     
     def count_solutions(self):
-        count = 0
-        for vector in make_perm(self.try_vector):
-            comb = self.create_combination(vector)
-            if self.debug_mode:
-                print("Trying", vector, comb)
-            count += self.is_one_solution(comb)
-        if self.debug_mode:
-            print("Final solutions")
-            for s in self.found_solutions:
-                print("".join(self.create_combination(s)))
-        return count
+        self.count = 0
+        
+        hash_c = count_in_list(self.try_vector,"#")
+        dot_c = count_in_list(self.try_vector,".")
+        length = len(self.try_vector)
+        
+        def perm_rec(v, hash_c, dot_c, length):
+            if len(v) == length:
+                comb = self.create_combination(v)
+                self.count += self.is_one_solution(comb)
+            if hash_c > 0:
+                perm_rec(v+["#"], hash_c-1, dot_c, length)
+            if dot_c > 0:
+                perm_rec(v+["."], hash_c, dot_c-1, length)
+        perm_rec([], hash_c, dot_c, length)
+            
+        return self.count
     
     def is_one_solution(self, result_vector):
         case = "".join(result_vector).split(".")
-        has_sizes = []
-        for c in case:
-            if len(c) > 0:
-                has_sizes.append(len(c))
-        if has_sizes == self.solution:
-            if result_vector not in self.found_solutions:
-                if self.debug_mode:
-                    print("Found", "".join(result_vector))
-                    print("     ", "".join(self.case))
-                    print("Sizes", has_sizes)
-                    print("Solution", self.solution)
-                self.found_solutions.append(result_vector)
-                return True
-        return False
-        
+        has_sizes = [len(c) for c in case if len(c) > 0]
+        if len(has_sizes) != len(self.solution):
+            return False
+        for i in range(len(has_sizes)):
+            if has_sizes[i] != self.solution[i]:
+                return False
+        return True
+    
     def create_combination(self, vector_comb):
         result = self.case.copy()
         i = 0
@@ -95,21 +82,55 @@ class HashCase:
         return result
 
 
-# from input_data import input_data
-input_data = """???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1"""
+from input_data import input_data
+# input_data = """???.### 1,1,3
+# .??..??...?##. 1,1,3
+# ?#?#?#?#?#?#?#? 1,3,1,6
+# ????.#...#... 4,1,1
+# ????.######..#####. 1,6,5
+# ?###???????? 3,2,1"""
 
 
-def get_result(x):
-    return x.count_solutions()
 
-total = 0
-cases = [HashCase(line) for line in input_data.splitlines()]
+# # print(HashCase("???.### 1,1,3").count_solutions())
+# cases = [HashCase(line) for line in input_data.splitlines()]
 
-with multiprocessing.Pool(processes=2) as pool:
+from functools import cache
+
+@cache
+def count(case, slots):
+    if case == "":
+        return 1 if slots == () else 0
+    
+    if slots == ():
+        return 0 if "#" in case else 1
+    
+    result = 0
+    
+    if case[0] in ".?":
+        result += count(case[1:], slots)
+    
+    if case[0] in "#?":
+        if slots[0] <= len(case) and "." not in case[:slots[0]] and (slots[0] == len(case) or case[slots[0]] != "#"):
+            result += count(case[slots[0]+1:], slots[1:])
+    
+    return result
+    
+cases = []
+for line in input_data.splitlines():
+    cfg, nums = line.split()
+
+    cfg = "?".join([cfg]*5)
+    nums += ","
+    nums *= 5
+    nums = nums[:-1]
+    
+    nums = tuple(map(int, nums.split(",")))
+    cases.append((cfg, nums))
+
+def get_result(case):
+    return count(case[0], case[1])
+
+with multiprocessing.Pool(processes=20) as pool:
     results = pool.map(get_result, cases)
 print(sum(results))
